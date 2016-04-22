8df0f8ce794ad09839b47241b71ed1cc9f123ac2
Lucas Stinchcombe <lucasstinchcombe@gmail.com>
2016-04-22 17:15:41 -0400

    Init tweetie

---
+++ tweetie/api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'vvM7sjlm9aCoM9MPipcV0wKKb');
+    define('CONSUMER_SECRET', 'FqiVaCwApfhShSU8WyxAULJ9ltt38oJ5Vw97WoMPxve3iwyGsz');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '2824599943-VizkbcTYOE74GsduIEZwSuPm5lKvmYAcPw4pzGu');
+    define('ACCESS_SECRET', 'tbl1QU6ucybIIyWzwwxmgYhS4GCyydW6durq5mswE8WFx');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
